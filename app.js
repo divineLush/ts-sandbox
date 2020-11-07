6d3bcfc8647cbc478a2ba82a66f486e6f6751055
Alexey <linux@localhost.localdomain>
2020-11-07 20:46:04 +0700

    some improvements

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 console.log('Here we go...');
 // CORE TYPES
-function add(n1, n2) {
-    return n1 + n2;
-}
+var add = function (n1, n2) { return n1 + n2; };
 console.log(add(2, 3), add('2', 3));
 // 5, '23'
-function addTyped(n1, n2) {
-    return n1 + n2;
-}
+var addTyped = function (n1, n2) { return n1 + n2; };
 // console.log(addTyped(2, 3), addTyped('2', 3))
 // 5, Error!
+var addImproved = function (n1, n2) {
+    var isNumber = function (n) { return typeof n === 'number'; };
+    if (isNumber(n1) && isNumber(n2)) {
+        return n1 + n2;
+    }
+    else {
+        throw new Error('Incorrect input');
+    }
+};
+console.log(addImproved('2', 3));
