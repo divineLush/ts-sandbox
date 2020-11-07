43b97027f9dc9034e772bf8cea28ff7842e473b1
Alexey <linux@localhost.localdomain>
2020-11-07 20:27:19 +0700

    using types...

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('Here we go...');
+// CORE TYPES
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(2, 3), add('2', 3));
+// 5, '23'
+function addTyped(n1, n2) {
+    return n1 + n2;
+}
+// console.log(addTyped(2, 3), addTyped('2', 3))
+// 5, Error!
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
 console.log('Here we go...')
+
+// CORE TYPES
+function add(n1, n2) {
+    return n1 + n2
+}
+
+console.log(add(2, 3), add('2', 3))
+// 5, '23'
+
+function addTyped(n1: number, n2: number) {
+    return n1 + n2
+}
+
+// console.log(addTyped(2, 3), addTyped('2', 3))
+// 5, Error!
